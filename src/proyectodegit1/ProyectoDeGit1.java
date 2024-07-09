84f25ce35cad38c48a09a84b22f3efa7510557ad
Javier Pérez <javiepr0104@gmail.com>
2024-07-09 07:26:29 -0600

    Creando repositorio

---
+++ src/proyectodegit1/ProyectoDeGit1.java
@@ -0,0 +1,13 @@
+package proyectodegit1;
+
+public class ProyectoDeGit1 {
+
+    public static void main(String[] args) {
+         System.out.println("Mi proyecto");
+         
+         for (int i = 0; i <= 10; i++) {
+             System.out.println(i);
+        }
+    }
+
+}
