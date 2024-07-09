ca6e87c6e36e316d94680e14d0433822dfa64b3d
Javier Pérez <javiepr0104@gmail.com>
2024-07-09 08:05:23 -0600

    se agrego otro ciclo

--- src/proyectodegit1/ProyectoDeGit1.java
+++ src/proyectodegit1/ProyectoDeGit1.java
@@ -3,10 +3,15 @@ package proyectodegit1;
 public class ProyectoDeGit1 {
 
     public static void main(String[] args) {
-         System.out.println("Mi proyecto");
-         
-         for (int i = 0; i <= 10; i++) {
-             System.out.println(i);
+        System.out.println("Mi proyecto");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+
+        }
+        //esto es otro ciclo de prueba 
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Bachillerato");
         }
     }
 
